209ed140bd5f4498950af1751a812f613f4cef7a
Rajani Kodavanti <rajanikodavantiemails@gmail.com>
2026-07-04 12:18:29 +0530

    for loop and print stmt are added

---
+++ iterate_list.py
@@ -0,0 +1,8 @@
+# This is to iterate a python list
+
+# list of numbers
+list_of_nubrs = [34535,35,676, 8898, 76]
+
+# iterate the numbers with for loop
+for numbers in list_of_nubrs:
+    print("list item: ", numbers)
